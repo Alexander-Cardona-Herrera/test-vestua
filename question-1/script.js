a97449f95bd91da45ab911e4f280330e14f343f8
Alexander-Cardona-Herrera <alexcardonah90@gmail.com>
2022-02-14 16:55:25 -0500

    initial files

---
+++ question-1/script.js
@@ -0,0 +1,12 @@
+/**
+ * On this module you should write your answer to question #1.
+ * This file would be executed with the following command (N=100):
+ * $ node scritp.js 100
+ */
+
+const args = process.argv.slice(-1);
+console.log(`Running question #1 with args ${args}`)
+
+function Algoritmo(args) {
+    if (typeof args !== )
+}
